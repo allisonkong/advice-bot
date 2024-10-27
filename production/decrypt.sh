8213a0508295840208504a67b44b18a47656979a
Allison Kong <allisonk2277@gmail.com>
2024-10-27 16:17:55 -0700

    move params to textproto format

--- production/decrypt.sh
+++ production/decrypt.sh
@@ -6,5 +6,5 @@ gcloud kms decrypt \
   --location=global \
   --keyring=secrets \
   --key=prod_secrets_encryption_key \
-  --ciphertext-file="./params.json.encrypted" \
-  --plaintext-file="./params.json"
+  --ciphertext-file="./params.textproto.encrypted" \
+  --plaintext-file="./params.textproto"
--- production/encrypt.sh
+++ production/encrypt.sh
@@ -6,5 +6,5 @@ gcloud kms encrypt \
   --location=global \
   --keyring=secrets \
   --key=prod_secrets_encryption_key \
-  --plaintext-file="./params.json" \
-  --ciphertext-file="./params.json.encrypted"
+  --plaintext-file="./params.textproto" \
+  --ciphertext-file="./params.textproto.encrypted"
